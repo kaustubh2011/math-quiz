6266366307e0717951874a38b7cdc0683bb63933
kaustubh2011 <81859002+kaustubh2011@users.noreply.github.com>
2021-07-31 17:04:59 +0530

    Add files via upload

--- game_page.js
+++ game_page.js
@@ -10,9 +10,54 @@ document.getElementById("player_question").innerHTML="question turn- "+player1_n
 document.getElementById("player_answer").innerHTML="answer turn- "+player2_name;
 function send()
  {
-      get_word = document.getElementById("word").value;
- word = get_word.toLowerCase(); 
- console.log("word in lowerCase = " + word);
- charAt1 = word.charAt(1);
-  console.log(charAt1);
+      Number1 = document.getElementById("num1").value;
+      Number2 = document.getElementById("num2").value;
+      new_answer=parseInt(Number1)*parseInt(Number2);
+         question_word = "<h4 id='word_display'> Q. "+Number1+"X"+Number2+"</h4>"; 
+         input_box = "<br>Answer : <input type='text' id='input_check_box'>";
+          check_button = "<br><br><button class='btn btn-info' onclick='check()'>Check</button>";
+           row = question_word + input_box + check_button ;
+            document.getElementById("output").innerHTML = row;
+             document.getElementById("word").value = "";
  }
+  question_turn="player1";
+  answer_turn="player2";
+  function check()
+  {
+       get_ans=document.getElementById("input_check_box").value;
+       ans=get_ans.toLowerCase();
+       if(ans==new_answer)
+       {
+            if(answer_turn=="player1")
+            {
+                 player1_score=player1_score+1;
+                 document.getElementById("player1_score").innerHTML=player1_score;
+            }
+            else
+            {
+               player2_score=player2_score+1;
+               document.getElementById("player2_score").innerHTML=player2_score;   
+            }
+       }
+       if(question_turn=="player1")
+       {
+            question_turn="player2";
+            document.getElementById("player_question").innerHTML="question turn- "+player2_name;
+       }
+       else
+       {
+          question_turn="player1";
+          document.getElementById("player_question").innerHTML="question turn- "+player1_name;
+       }
+       if(answer_turn=="player1")
+       {
+            answer_turn="player2";
+            document.getElementById("player_answer").innerHTML="answer turn- "+player2_name;
+       }
+       else
+       {
+          answer_turn="player1";
+          document.getElementById("player_answer").innerHTML="answer turn- "+player1_name;
+       } 
+       document.getElementById("output").innerHTML="";
+  }
